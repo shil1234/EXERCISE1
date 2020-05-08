d0df91d62a0e2a0d5ef7a70978cf9d170748739f
Administrator <18350108386@163.com>
2020-05-08 18:10:09 +0800

    test

---
+++ 机器学习/KNN/knn.py
@@ -0,0 +1,16 @@
+from numpy import  *
+
+import operator
+
+def createDataSet():
+
+    group=array([[1.2,1.3],[1.1,1.2],[3.0,3.0],[3.2,3.4]])
+    labels=['A','A','B','B']
+    return  group,labels
+
+
+
+
+
+
+
